01bbfd0c165170bc06e836008f62fe16daa3e7ac
Eduardo Suarez <draco_008@hotmail.com>
2025-12-28 14:16:22 -0600

    code arrangement

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let score = document.querySelector('.score');
 let numberScore = Number(score.textContent);
 let highScore = document.querySelector('.highscore');
 let again = document.querySelector('.again');
+let actualHighscore = 0;
 //console.log(number)
 
 //logic of the check buttom.
@@ -22,19 +23,23 @@ check.addEventListener('click', function() {
     }
 
     //The basic logic of the game
+    //When players is too high
     if (guess > randomNumber){
         console.log('te pasaste')
         numberScore -= 1
         score.textContent = numberScore
         message.textContent = 'You are to high'
         console.log(score)
+    //When players wins
     }else if (guess === randomNumber){
         number.textContent = randomNumber;
-        message.textContent = 'Congrats, you find the number';
-        message.style.color = 'green'
+        message.textContent = 'Congrats, you find the correct number!!!';
+        message.style.color = 'white'
         message.style.fontSize = '25px'
         highScore.textContent = score.textContent;
+        document.querySelector('body').style.backgroundColor = 'green'
         console.log('le atinaste')
+    //When players is too low
     }else{
         console.log('te falta we')
         numberScore -= 1
@@ -49,9 +54,11 @@ again.addEventListener('click' , function () {
     //Generate a new number
     randomNumber = Math.floor(Math.random() * 20) + 1;
 
+    document.querySelector('body').style.backgroundColor = "#222";
+
     //Reset score
     numberScore = 20;
-    score = numberScore;
+    score.textContent = numberScore;
 
     //Reset messagues
     message.textContent = 'Start Guessing...';
